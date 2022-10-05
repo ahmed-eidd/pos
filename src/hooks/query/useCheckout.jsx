f70f79f053f65657c3fbbeffd442fc615cedddc1
Ahmed Eid <ahmedeid.webdev@gmail.com>
2022-10-05 18:27:09 +0200

    changed checkout query invalidations

--- src/hooks/query/useCheckout.jsx
+++ src/hooks/query/useCheckout.jsx
@@ -1,13 +1,18 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { message } from 'antd';
 import { useNavigate } from 'react-router-dom';
+import { queryKeys } from '../../constants/queryKeys';
 import { axiosInstance } from '../../service/api';
+import { useZusStore } from '../../store/useStore';
 
 export const usePayOrder = () => {
   const navigate = useNavigate();
+  const posId = useZusStore((state) => state.auth.posId);
+  const queryClient = useQueryClient();
   return useMutation(
     (data) => {
       const body = new FormData();
+      body.append('point_of_sale_id', posId);
       for (let [key, value] of Object.entries(data)) {
         body.append(key, value);
       }
@@ -20,6 +25,8 @@ export const usePayOrder = () => {
           return;
         }
         navigate('/order-placed');
+        queryClient.invalidateQueries([queryKeys.getCart]);
+        queryClient.invalidateQueries([queryKeys.getProducts]);
       },
     }
   );
