5d5e93de5f39778ab1ab1cd901bdafb3f55f0285
Ahmed Eid <a.eid@taqneen.com>
2023-11-10 02:05:35 +0200

    Add pagination and table to DataLowestModal component

--- src/api-hooks/useDataLowest.jsx
+++ src/api-hooks/useDataLowest.jsx
@@ -2,17 +2,20 @@ import { useQuery } from '@tanstack/react-query';
 import useApi from './useApi';
 import { queryKeys } from '../services/react-query/queryKeys';
 
-export const useGetDataLowest = () => {
+export const useGetDataLowest = ({ page, open }) => {
   const api = useApi();
   const http = async () => {
     // const res = await api.post(`getClientsForHotel`, fd);
-    const res = await api.post(`/dataLowest`);
+    const res = await api.post(`/dataLowest`, {
+      page: page,
+    });
     return res;
   };
 
-  return useQuery([queryKeys.getDataLowest], http, {
+  return useQuery([queryKeys.getDataLowest, page], http, {
     onSuccess: (res) => {
       return res;
     },
+    enabled: open,
   });
 };
--- src/components/Layout/CartSidebar/CartHeader/DataLowestModal.jsx
+++ src/components/Layout/CartSidebar/CartHeader/DataLowestModal.jsx
@@ -1,10 +1,21 @@
-import { Modal, Spin } from 'antd';
-import React, { useEffect } from 'react';
+import { Modal, Spin, Table } from 'antd';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useGetDataLowest } from '../../../../api-hooks/useDataLowest';
+import './DataLowestModal.scss';
 
 const DataLowestModal = ({ open, setOpen }) => {
-  const { data, refetch, isFetching } = useGetDataLowest();
-  console.log(data);
+  const [currnetPage, setCurrentPage] = useState(1);
+  const { data, refetch, isFetching } = useGetDataLowest({
+    page: currnetPage,
+    open,
+  });
+  const dataFormated = useMemo(() => {
+    const items = data?.data?.map((item) => ({
+      ...item?.ingredient,
+      name: item?.ingredient?.name?.ar,
+    }));
+    return items;
+  }, [data]);
 
   useEffect(() => {
     if (open) {
@@ -17,8 +28,41 @@ const DataLowestModal = ({ open, setOpen }) => {
       footer={null}
       onCancel={() => setOpen(false)}
       destroyOnClose
+      width={800}
     >
-      <Spin spinning={isFetching}>لاتوجد بيانات للعرض</Spin>
+      <Spin spinning={isFetching}>
+        <Table
+          pagination={{
+            total: data?.total,
+            onChange: (page, pageSize) => {
+              setCurrentPage(page);
+            },
+          }}
+          columns={[
+            {
+              title: 'الاسم',
+              dataIndex: 'name',
+            },
+            {
+              title: 'التكلفة',
+              dataIndex: 'cost',
+            },
+            {
+              title: 'السعر',
+              dataIndex: 'price',
+            },
+            {
+              title: 'الكمية',
+              dataIndex: 'stock',
+            },
+            {
+              title: 'اخر سعر للبيع',
+              dataIndex: 'last_selling_price',
+            },
+          ]}
+          dataSource={dataFormated}
+        ></Table>
+      </Spin>
     </Modal>
   );
 };
